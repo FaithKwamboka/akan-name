3460abddef7eb2ffdb31a80c788415e7785cbac6
FaithKwamboka <okongofaith3@gmail.com>
2022-03-05 02:53:46 +0300

    define JS variables and functions

--- js/script.js
+++ js/script.js
@@ -28,3 +28,59 @@ let weekDay = [
 ];
 
 document.getElementById("output").style.display = "none";
+
+function onSubmit(){
+    let day = daysOfTheWeek[calculateDay()];
+    let name = getAkanName();
+    document.getElementById("day").innerHTML = day;
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("output").style.display = "block";
+}
+
+function getYear() {
+    let year = document.getElementById("year").value;
+    return year;
+}
+
+function getMonth() {
+    let month = document.getElementById("month").value;
+    return month;
+}
+
+function getDate() {
+    let date = document.getElementById("day_").value;
+    return date;
+}
+
+function getGender() {
+    if (document.getElementById("male").checked == true) {
+        male = "male"
+    };
+    if (document.getElementById("female").checked == true) {
+        female = "female"
+    };
+}
+
+function calculateDay() {
+    let year = parseInt(getYear());
+    let MM = parseInt(getMonth());
+    let DD = parseInt(getDate());
+
+    let CC = year.slice(0, 2);
+    let YY = year.slice(2, 4);
+
+    let dayOfWeek = Math.floor((((Number(CC) / 4) - 2 * Number(CC) - 1) + ((5 * Number(YY) / 4)) + ((26 * (Number(MM) + 1) / 10)) + Number(DD)) % 7);
+    console.log dayOfWeek;
+}
+
+function getAkanName() {
+    let gender = getGender();
+    let d = calculateDay();
+    let akanName = "";
+    if (gender === "male") {
+        akanName = maleAkanName[d];
+    } else {
+        akanName = femaleAkanName[d];
+    }
+    return akanName
+}
