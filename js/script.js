6bb840e86ffad074c9a95adec821f5bb560c733d
FaithKwamboka <okongofaith3@gmail.com>
2022-03-04 19:00:47 +0300

    assign variables

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let maleAkanName = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+];
+
+let femaleAkanName = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+
+let weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
